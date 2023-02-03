372e683c720ab19040abbaa370062873006deac5
Luis Höfer <luis.hoefer@gmail.com>
2023-02-03 07:14:47 +0100

    chore: install tailwind and daisyui

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,12 @@
 import { defineConfig } from "astro/config";
 
 // https://astro.build/config
+import tailwind from "@astrojs/tailwind";
+
+// https://astro.build/config
 export default defineConfig({
   server: {
-    port: 9000,
+    port: 9000
   },
+  integrations: [tailwind()]
 });
